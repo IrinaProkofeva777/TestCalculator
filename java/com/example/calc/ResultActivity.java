3c4efa03071ebd9e13698fe630fd5c9dd7961b9f
IrinaProkofeva777 <72920141+IrinaProkofeva777@users.noreply.github.com>
2021-03-29 12:19:16 +0300

    Update ResultActivity.java

--- java/com/example/calc/ResultActivity.java
+++ java/com/example/calc/ResultActivity.java
@@ -7,35 +7,28 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-/**
- *  Класс создания нового активити с результатами операации
- */
+
 public class ResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        // заявление макета
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LinearLayout linearLayout = new LinearLayout(this);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
         linearLayout.setGravity(Gravity.CENTER);
 
-        // задание текстового элемента
         TextView textView = new TextView(this);
         textView.setTextSize(50);
         textView.setGravity(Gravity.CENTER);
-
-        //добаваляем в activity_main макет и в макет всьавляем текст вьюшку
         setContentView(linearLayout);
         linearLayout.addView(textView);
 
-        //извлекааем данные при помощь getIntent() и присваиваем данные из прошлого активити
         Intent intent = getIntent();
         String firstText = intent.getStringExtra("firstText");
         String secondtext = intent.getStringExtra("secondText");
 
-        // Выводим результаты операций
         String result = intent.getStringExtra("result");
         String finalText = firstText + " + " + secondtext + " = " + result;
         textView.setText(finalText);
